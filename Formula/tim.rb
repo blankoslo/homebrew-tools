ec0c0b58b66317f1fb29550549c53b4bae737060
John Korsnes <john.korsnes@blank.no>
2025-11-22 18:06:22 +0100

    Update to 0.0.11

--- Formula/tim.rb
+++ Formula/tim.rb
@@ -1,22 +1,22 @@
 class Tim < Formula
   desc "tim - timeføring cli"
   homepage "https://github.com/blankoslo/tim"
-  version "0.1.0"
+  version "0.0.11"
 
   if OS.mac?
     unless Hardware::CPU.arm?
       odie "tim is only available for macOS arm64 currently."
     end
 
-    url "https://github.com/blankoslo/tim/releases/download/0.1.0/tim-macos.tar.gz"
-    sha256 "01cabc62beb1c07087e8060a3fb705efe0f24b80e7a6675614289bc8ff11a320"
+    url "https://github.com/blankoslo/tim/releases/download/0.0.11/tim-macos.tar.gz"
+    sha256 "3303c7b9e68a5b4bfa8379304a17cca8f2c7accbca6b67d196732264c89128ea"
   elsif OS.linux?
     unless Hardware::CPU.intel?
       odie "tim is only available for Linux x86_64 currently."
     end
     
-    url "https://github.com/blankoslo/tim/releases/download/0.1.0/tim-linux.tar.gz"
-    sha256 "8c05b1a7b4530e50f1e73fb1a017dde95ae2fcc36e6daa386bed8161dcb0f8f3"
+    url "https://github.com/blankoslo/tim/releases/download/0.0.11/tim-linux.tar.gz"
+    sha256 "5793ee21eb2ba2e05886f216c73e14d9102224e69336a500418b67e668c92052"
   end
 
   def install

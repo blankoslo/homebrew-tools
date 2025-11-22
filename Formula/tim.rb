608a7e9e8c99d5769a1462f7321e18832aad01e5
John Korsnes <john.korsnes@blank.no>
2025-11-23 00:18:18 +0100

    tim-formula:0.0.12

--- Formula/tim.rb
+++ Formula/tim.rb
@@ -1,22 +1,22 @@
 class Tim < Formula
-  desc "tim - timeføring cli"
+  desc "tim - floq cli"
   homepage "https://github.com/blankoslo/tim"
-  version "0.0.11"
+  version "0.0.12"
 
   if OS.mac?
     unless Hardware::CPU.arm?
       odie "tim is only available for macOS arm64 currently."
     end
 
-    url "https://homebrewfiles.blob.core.windows.net/tim/0.0.11/tim-osx.tar.gz"
-    sha256 "3303c7b9e68a5b4bfa8379304a17cca8f2c7accbca6b67d196732264c89128ea"
+    url "https://homebrewfiles.blob.core.windows.net/tim/0.0.12/tim-osx.tar.gz"
+    sha256 "adffd5592574d46429171bc553565ab7ac69a9f87f4dc51f71e5fc723a3714fb"
   elsif OS.linux?
     unless Hardware::CPU.intel?
       odie "tim is only available for Linux x86_64 currently."
     end
     
-    url "https://homebrewfiles.blob.core.windows.net/tim/0.0.11/tim-linux.tar.gz"
-    sha256 "5793ee21eb2ba2e05886f216c73e14d9102224e69336a500418b67e668c92052"
+    url "https://homebrewfiles.blob.core.windows.net/tim/0.0.12/tim-linux.tar.gz"
+    sha256 "7d69e5a554793199db217a16b9ac807e4f4de3d331a9e63e5419566ff979602a"
   end
 
   def install

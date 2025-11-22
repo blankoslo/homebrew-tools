c305f44816b6576781d43bb05ddc6106c99cdde6
John Korsnes <john.korsnes@blank.no>
2025-11-22 21:43:33 +0100

    tim: update brew-download urls to public endpoints

--- Formula/tim.rb
+++ Formula/tim.rb
@@ -5,17 +5,17 @@ class Tim < Formula
 
   if OS.mac?
     unless Hardware::CPU.arm?
-      odie "tim is only available for osx arm64 currently."
+      odie "tim is only available for macOS arm64 currently."
     end
 
-    url "https://github.com/blankoslo/tim/releases/download/0.0.11/tim-osx.tar.gz"
+    url "https://homebrew.blank.no/blankoslo/tim/releases/download/0.0.11/tim-osx.tar.gz"
     sha256 "3303c7b9e68a5b4bfa8379304a17cca8f2c7accbca6b67d196732264c89128ea"
   elsif OS.linux?
     unless Hardware::CPU.intel?
       odie "tim is only available for Linux x86_64 currently."
     end
     
-    url "https://github.com/blankoslo/tim/releases/download/0.0.11/tim-linux.tar.gz"
+    url "https://homebrew.blank.no/blankoslo/tim/releases/download/0.0.11/tim-linux.tar.gz"
     sha256 "5793ee21eb2ba2e05886f216c73e14d9102224e69336a500418b67e668c92052"
   end
 

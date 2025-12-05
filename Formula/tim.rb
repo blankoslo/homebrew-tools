4cea61c8e1beac4cdb7f8554f9a1f97c21277e07
John Korsnes <john.korsnes@blank.no>
2025-12-05 10:50:28 +0100

    tim-v0.2.4

--- Formula/tim.rb
+++ Formula/tim.rb
@@ -1,25 +1,29 @@
 class Tim < Formula
   desc "tim - floq cli"
   homepage "https://github.com/blankoslo/tim"
-  version "0.2.3"
+  version "0.2.4"
 
   if OS.mac?
     unless Hardware::CPU.arm?
       odie "tim is only available for macOS arm64 currently."
     end
 
-    url "https://homebrewfiles.blob.core.windows.net/tim/0.2.3/tim-osx.tar.gz"
-    sha256 "d453963d52ba93372a0b62aac416120865b1e277f98dc9532b3527aa77594c46"
+    url "https://homebrewfiles.blob.core.windows.net/tim/#{version}/tim-osx.tar.gz"
+    sha256 "fcd1ac3e41f7311dc65581df40029c111490068b455e3da21d6d7e31971fb5db"
   elsif OS.linux?
     unless Hardware::CPU.intel?
       odie "tim is only available for Linux x86_64 currently."
     end
 
-    url "https://homebrewfiles.blob.core.windows.net/tim/0.2.3/tim-linux.tar.gz"
-    sha256 "afc8fbc81af4c2d78f76a440f12b01bfe69697c5a349927298fc56ca0257926d"
+    url "https://homebrewfiles.blob.core.windows.net/tim/#{version}/tim-linux.tar.gz"
+    sha256 "45cef902e4153ff6feec8a7329b60d53642d1b95cee0c50bdd486833530c7fcb"
   end
 
   def install
     bin.install "tim"
+  end
+  
+  test do
+    system "#{bin}/tim", "--version"
   end
 end
